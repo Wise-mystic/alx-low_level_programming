eea982f85f2c5fb6d2dbd0f686608cebd8f13595
Wise-mystic <wisdomkwawuvi23@gamil.com>
2023-09-07 12:51:15 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <sdtio.h>
+/**
+ * main - A program that prints a line using the printf funtion
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n);
+	retun (0);
+}
