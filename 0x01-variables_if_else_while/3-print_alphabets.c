52f9e5d962255f723fac0d5946ac66ab5cd02d3c
Wise-mystic <wisdomkwawuvi23@gamil.com>
2023-09-08 12:43:01 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Point of entry
+ * Return: Always return 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'A';
+	d = 'a';
+	while
+		(c <= 'Z') {
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'z'); {
+			putchar(d);
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
