c76915e18ea3a76d36fcc6b2e3f37695591f17a1
Wise-mystic <wisdomkwawuvi23@gamil.com>
2023-09-08 11:49:07 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Point of entry
+ * Return: Always return 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'w';
+	while
+		(c <= "w") {
+			patchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
