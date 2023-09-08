32798d7e5741371ec12e1a0a31397af32118683c
Wise-mystic <wisdomkwawuvi23@gamil.com>
2023-09-08 13:41:23 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prog Point of entry
+ * Return: Always return 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0 < 10; a++)
+		printf("%d", a);
+	putchar('\n');
+	return (0);
+}
