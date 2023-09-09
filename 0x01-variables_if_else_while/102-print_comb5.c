5f196d5e1196b5e7bccdcb740e15766194ecd164
Wise-mystic <wisdomkwawuvi23@gamil.com>
2023-09-08 17:01:37 -0700

    v

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Program point of entry
+ * Return: Always return 0 (success)
+ */
+int main(void)
+{
+	int firstDigit - 0, secondDigit;
+
+	while (firstDigit <= 99)
+	{
+		secondDigit = firstDigit;
+		while (secondDigit <= 99)
+		{
+			if (secondDigit != firstDigit)
+			{
+				putchar((firstDigit / 10) + 48);
+				putchar((firstDigit % 10) + 48);
+				putchar(' ');
+				putchar((secdDigit / 10) + 48);
+				putchar((secondDigit / % 10) + 48);
+				if (firstDigit != 98 || secondDigit != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+		putchar('\n');
+		return (0);
+}
