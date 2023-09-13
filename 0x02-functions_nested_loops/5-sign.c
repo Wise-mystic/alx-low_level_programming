e3282deb1d463da49d341388dc8406efec357cb5
Wise-mystic <wisdomkwawuvi23@gamil.com>
2023-09-12 22:27:31 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+
+/**
+  * main - A function that prints the sings of numbers
+  * Return: Always 0.
+  */
+
+int main(void)
+
+{
+	int r;
+
+	r = print_sign(98);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+	r = print_sign(0);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+	r = print_sign(0xff);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+	r = print_sign(-1);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
+}
+
