0384a508c75223e661cd282460e356daf374941c
Wise-mystic <wisdomkwawuvi23@gamil.com>
2023-09-12 23:11:27 -0700

    #include "main.h" /**  * _islower - entry point  * Description: function that checks for lowercase character  * @c:character to check if it islowercase  * Return: 0-if lowercase, 1-if not  */
    int _islower(int c)
    {
    if (c >= a && c <= z)
    {
    return (1);
    }
    else
    {
    return (0);
    }
    }

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,20 +1,20 @@
 #include "main.h"
-
 /**
- * main  -A fuction that deals with islower
- * Return: Always 0.
+ * _islower - entry point
+ * Description: function that checks for lowercase character
+ * @c:character to check if it islowercase
+ * Return: 0-if lowercase, 1-if not
  */
-int main(void)
-{
-	int r;
 
-	r = _islower('H');
-	_putchar(r + '0');
-	r = _islower('o');
-	_putchar(r + '0');
-	r = _islower(108);
-	_putchar(r + '0');
-	_putchar('\n');
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+	return (1);
+	}
+	else
+	{
 	return (0);
+	}
 }
 
