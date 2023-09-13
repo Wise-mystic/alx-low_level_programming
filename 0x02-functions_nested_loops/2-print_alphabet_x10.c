c9404e063d6f54742ad4a22597a60f9e808db369
Wise-mystic <wisdomkwawuvi23@gamil.com>
2023-09-12 22:04:39 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabest 10 times more
+ * Return: void here
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int d = 0;
+
+		while (d <= 9)
+		{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+			_putchar('\n');
+			i++;
+		}
+}
+
