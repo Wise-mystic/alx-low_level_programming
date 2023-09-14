b5f1ee68b4fe24fb325491aea3f27b6697bf846e
Wise-mystic <wisdomkwawuvi23@gamil.com>
2023-09-13 23:00:02 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -4,7 +4,7 @@ int main(void)
 {
     int sum = 0;
 
-    for (int i = 3; i < 1024; i++) {
+    while (int i = 3; i < 1024; i++) {
         if (i % 3 == 0 || i % 5 == 0) {
             sum += i;
         }
