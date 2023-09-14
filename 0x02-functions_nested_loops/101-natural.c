533131e141ffe626d7a46f8284770bd03688f592
Wise-mystic <wisdomkwawuvi23@gamil.com>
2023-09-13 23:02:54 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,13 +2,20 @@
 
 int main(void)
 {
+    int limit = 1024;
     int sum = 0;
 
-    while (int i = 3; i < 1024; i++) {
-        if (i % 3 == 0 || i % 5 == 0) {
-            sum += i;
-        }
-    }
+    // Calculate the sum of multiples of 3 below the limit
+    int sum_of_3 = (3 * ((limit - 1) / 3) * (((limit - 1) / 3) + 1)) / 2;
+
+    // Calculate the sum of multiples of 5 below the limit
+    int sum_of_5 = (5 * ((limit - 1) / 5) * (((limit - 1) / 5) + 1)) / 2;
+
+    // Calculate the sum of multiples of 15 (to avoid double counting)
+    int sum_of_15 = (15 * ((limit - 1) / 15) * (((limit - 1) / 15) + 1)) / 2;
+
+    // Calculate the final sum
+    sum = sum_of_3 + sum_of_5 - sum_of_15;
 
     printf("The sum of multiples of 3 or 5 below 1024 is: %d\n", sum);
 
