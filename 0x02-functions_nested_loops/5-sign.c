f6a4d06d56adf5fa615e6e9b4ef019eb9c22104d
Wise-mystic <wisdomkwawuvi23@gamil.com>
2023-09-13 20:43:47 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,36 +1,26 @@
-#include "main.h"
-
+ #include "main.h"
 
 /**
-  * main - A function that prints the sings of numbers
-  * Return: Always 0.
-  */
-
-int main(void)
-
+ * print_sign - Printys sum
+ * @n: The nmber
+ * Return: 1 for all
+ */
+int print_sign(int n)
 {
-	int r;
-
-	r = print_sign(98);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(0);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(0xff);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(-1);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
 }
 
