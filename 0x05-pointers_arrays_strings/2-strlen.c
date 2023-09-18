ce009b80d252fe5dc1eb269a97cb598e656eaa7c
Wise-mystic <wisdomkwawuvi23@gamil.com>
2023-09-18 16:12:28 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - a functions that returns the lenght of a string
+ * @s: strings
+ * Return: Lenght
+ */
+
+int _strlen(char *s)
+
+{
+	int longi = 0;
+
+	while (*s != '\0')
+	{
+	longi++;
+	s++
+	}
+
+	return (longi);
+}
