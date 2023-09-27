c472866d0c2d50115d7496d8660a9d459baa33f2
Wise-mystic <wisdomkwawuvi@gmail.com>
2023-09-26 20:10:10 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - this is the entry point
+ * @s: input
+ * @to: input
+ * Return: Always retun 0 (success)
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
