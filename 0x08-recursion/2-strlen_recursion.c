13abf7bf4aad10a2a766fbb02954b7ae0ef76e6e
Wise-mystic <wisdomkwawuvi@gmail.com>
2023-09-28 21:41:51 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,18 +1,21 @@
 #include "main.h"
-/**
- * _strlen_recursion - A function that returns the length of a sting
- * @s: the string to be measured
- * Return: the length of the string
- */
 
+/**
+  * _strlen_recursion - main function
+  *
+  * @s: Function parameter
+  *
+  * Return: Length
+  */
 int _strlen_recursion(char *s)
 {
-	int longit - 0;
+	int lnt = 0;
 
 	if (*s)
 	{
-		longit++;
-		longit += _strlen_recursion(s + 1);
+	lnt++;
+	lnt += _strlen_recursion(s + 1);
 	}
-	return (longit);
+	return (lnt);
 }
+
